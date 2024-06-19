64cb278893b5b8454b606dfb17421b2012bd93c5
FCJar <Fcjardim764@gmail.com>
2024-06-18 22:39:25 -0300

    0.0.6

--- games/menu.py
+++ games/menu.py
@@ -2,6 +2,7 @@ import pygame
 import sys 
 import os
 import includes.colors as colors
+from includes.Player import Player
 
 current_path = os.path.dirname(__file__)
 image_path = os.path.join(current_path, '..', 'assets', 'images', 'main_background.jpg')
@@ -15,6 +16,8 @@ class MainMenu:
         self.screen_height = screen_height
         self.running = True
         self.selection = None
+        self.Pl=Player('Jose',15000)
+        self.Pl.readData()
 
     def run(self):
         while self.running:
@@ -41,10 +44,13 @@ class MainMenu:
     def draw(self):
         fonte_base = pygame.font.Font(None, 36)
         fonte_titulo = pygame.font.Font(None, 72)
+        fonte_player = pygame.font.Font(None,36)
         self.screen.fill(colors.WHITE_COLOR)
         self.screen.blit(main_background,(0,0))
         text = fonte_titulo.render("Welcome to Cassino", True, colors.BLUE_COLOR)
         self.screen.blit(text,(self.screen_width//2 - text.get_width()//2, self.screen_height//2 - text.get_height()//2- 150))
+        text=fonte_player.render(("Player name:: " + self.Pl.get_name() + "      Actual chips:: " + str(self.Pl.get_chips())),True,colors.BOARD_COLOR)
+        self.screen.blit(text,(self.screen_width//2 - text.get_width()//2, self.screen_height//2 - text.get_height()//2))
         text = fonte_base.render("Press 1 to play Blackjack", True, colors.GREEN_COLOR)
         self.screen.blit(text, (self.screen_width//2 - text.get_width()//2, self.screen_height//2 - text.get_height()//2 + 50))
         text = fonte_base.render("Press 2 to play lucky roulette", True, colors.GREEN_COLOR)
